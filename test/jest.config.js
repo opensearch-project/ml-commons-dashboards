985f4c999a2d9bef41930a6766982d0a4b88edec
Lin Wang <wonglam@amazon.com>
2022-09-22 15:00:31 +0800

    add test:jest script and unit-tests-workflow
    Signed-off-by: Lin Wang <wonglam@amazon.com>

---
+++ test/jest.config.js
@@ -0,0 +1,21 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+module.exports = {
+  rootDir: '../',
+  setupFiles: ['<rootDir>/test/setupTests.ts'],
+  setupFilesAfterEnv: ['<rootDir>/test/setup.jest.ts'],
+  roots: ['<rootDir>'],
+  moduleNameMapper: {
+    '\\.(css|less|scss)$': '<rootDir>/test/mocks/styleMock.ts',
+    '^ui/(.*)': '<rootDir>/../../src/legacy/ui/public/$1/',
+  },
+  testMatch: ['**/*.test.{js,mjs,ts,tsx}'],
+  testPathIgnorePatterns: ['<rootDir>/target/', '<rootDir>/node_modules/'],
+  transform: {
+    '^.+\\.(js|tsx?)$': '<rootDir>/../../src/dev/jest/babel_transform.js',
+  },
+  testEnvironment: 'jsdom',
+};
---
+++ test/setup.jest.ts
@@ -0,0 +1,8 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { configure } from '@testing-library/react';
+
+configure({ testIdAttribute: 'data-test-subj' });
