1cc574edf13ffbafef888284206aed1bae737829
Lin Wang <wonglam@amazon.com>
2022-12-15 11:32:29 +0800

    Add model list filter ui (#23)
    * feat: add model list filter ui
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>
    
    * refactor: remove algorithm context selector
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>
    
    * refactor: update fireEvent to userEvent and displayName to name
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>

--- public/components/model_list/index.tsx
+++ public/components/model_list/index.tsx
@@ -6,7 +6,6 @@
 import React, { useState, useCallback, useMemo, useRef } from 'react';
 import { EuiPageHeader, EuiButton, EuiSpacer, EuiPanel } from '@elastic/eui';
 import { Link } from 'react-router-dom';
-import moment from 'moment';
 
 import { CoreStart } from '../../../../../src/core/public';
 import { APIProvider } from '../../apis/api_provider';
@@ -14,7 +13,7 @@ import { routerPaths } from '../../../common/router_paths';
 import { useFetcher } from '../../hooks/use_fetcher';
 
 import { ModelTable } from './model_table';
-import { ModelListFilter } from './model_list_filter';
+import { ModelListFilter, ModelListFilterFilterValue } from './model_list_filter';
 import {
   ModelConfirmDeleteModal,
   ModelConfirmDeleteModalInstance,
@@ -24,24 +23,22 @@ import { ModelDrawer } from '../model_drawer';
 export const ModelList = ({ notifications }: { notifications: CoreStart['notifications'] }) => {
   const confirmModelDeleteRef = useRef<ModelConfirmDeleteModalInstance>(null);
   const [params, setParams] = useState<{
-    algorithms?: string[];
-    context?: { [key: string]: Array<string | number> };
-    trainedStart?: moment.Moment | null;
-    trainedEnd?: moment.Moment | null;
     sort: 'trainTime-desc' | 'trainTime-asc';
     currentPage: number;
     pageSize: number;
+    filterValue: ModelListFilterFilterValue;
   }>({
     currentPage: 1,
     pageSize: 15,
     sort: 'trainTime-desc',
+    filterValue: { tag: [], owner: [], stage: [] },
   });
   const [drawerModelName, setDrawerModelName] = useState('');
   const { data, reload } = useFetcher(APIProvider.getAPI('model').search, {
-    ...params,
+    pageSize: params.pageSize,
+    currentPage: params.currentPage,
+    name: params.filterValue.search,
     sort: undefined,
-    trainedStart: params.trainedStart?.valueOf(),
-    trainedEnd: params.trainedEnd?.valueOf(),
   });
   const models = useMemo(() => data?.data || [], [data]);
   const totalModelCounts = data?.pagination.totalRecords || 0;
@@ -59,29 +56,6 @@ export const ModelList = ({ notifications }: { notifications: CoreStart['notific
     reload();
     notifications.toasts.addSuccess('Model has been deleted.');
   }, [reload, notifications.toasts]);
-
-  const handleAlgorithmsChange = useCallback(
-    (algorithms: string | undefined) => {
-      setParams((previousValue) => ({
-        ...previousValue,
-        algorithms: algorithms ? [algorithms] : undefined,
-        context: undefined,
-      }));
-    },
-    [setParams]
-  );
-
-  const handleContextChange = useCallback((context) => {
-    setParams((previousValue) => ({ ...previousValue, context }));
-  }, []);
-
-  const handleTrainedStartChange = useCallback((date: moment.Moment | null) => {
-    setParams((previousValue) => ({ ...previousValue, trainedStart: date }));
-  }, []);
-
-  const handleTrainedEndChange = useCallback((date: moment.Moment | null) => {
-    setParams((previousValue) => ({ ...previousValue, trainedEnd: date }));
-  }, []);
 
   const handleModelDelete = useCallback((modelId: string) => {
     confirmModelDeleteRef.current?.show(modelId);
@@ -113,6 +87,10 @@ export const ModelList = ({ notifications }: { notifications: CoreStart['notific
     });
   }, []);
 
+  const handleFilterChange = useCallback((filterValue: ModelListFilterFilterValue) => {
+    setParams((prevValue) => ({ ...prevValue, filterValue, currentPage: 1 }));
+  }, []);
+
   return (
     <EuiPanel>
       <EuiPageHeader
@@ -134,16 +112,7 @@ export const ModelList = ({ notifications }: { notifications: CoreStart['notific
         bottomBorder
       />
       <EuiSpacer />
-      <ModelListFilter
-        context={params.context}
-        algorithm={params.algorithms?.[0]}
-        onContextChange={handleContextChange}
-        onAlgorithmsChange={handleAlgorithmsChange}
-        trainedStart={params.trainedStart}
-        trainedEnd={params.trainedEnd}
-        onTrainedStartChange={handleTrainedStartChange}
-        onTrainedEndChange={handleTrainedEndChange}
-      />
+      <ModelListFilter value={params.filterValue} onChange={handleFilterChange} />
       <EuiSpacer />
       <ModelTable
         models={models}
--- public/components/model_list/model_list_filter.tsx
+++ public/components/model_list/model_list_filter.tsx
@@ -3,51 +3,69 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { EuiFlexItem, EuiFlexGroup } from '@elastic/eui';
-import moment from 'moment';
-import React from 'react';
+import { EuiFlexItem, EuiFlexGroup, EuiFieldSearch, EuiFilterGroup } from '@elastic/eui';
+import React, { useCallback, useRef } from 'react';
 
-import { RangePicker } from '../range_picker';
+import { TagFilter } from './tag_filter';
+import { OwnerFilter } from './owner_filter';
+import { StageFilter } from './stage_filter';
 
-import { AlgorithmSelector } from './algorithm_selector';
-import { ContextSelector } from './context_selector';
+export interface ModelListFilterFilterValue {
+  search?: string;
+  tag: string[];
+  owner: string[];
+  stage: string[];
+}
 
 export const ModelListFilter = ({
-  context,
-  onContextChange,
-  algorithm,
-  onAlgorithmsChange,
-  trainedStart,
-  onTrainedStartChange,
-  trainedEnd,
-  onTrainedEndChange,
+  value,
+  onChange,
+  defaultSearch,
 }: {
-  algorithm: string | undefined;
-  context: { [key: string]: Array<string | number> } | undefined;
-  trainedStart: moment.Moment | null | undefined;
-  trainedEnd: moment.Moment | null | undefined;
-  onAlgorithmsChange: (algorithms: string | undefined) => void;
-  onContextChange: (context: { [key: string]: Array<string | number> } | undefined) => void;
-  onTrainedStartChange: (time: moment.Moment | null) => void;
-  onTrainedEndChange: (time: moment.Moment | null) => void;
+  defaultSearch?: string;
+  value: Omit<ModelListFilterFilterValue, 'search'>;
+  onChange: (value: ModelListFilterFilterValue) => void;
 }) => {
+  const valueRef = useRef(value);
+  valueRef.current = value;
+  const onChangeRef = useRef(onChange);
+  onChangeRef.current = onChange;
+
+  const handleSearch = useCallback((search) => {
+    onChangeRef.current({ ...valueRef.current, search });
+  }, []);
+
+  const handleTagChange = useCallback((tag: string[]) => {
+    onChangeRef.current({ ...valueRef.current, tag });
+  }, []);
+
+  const handleOwnerChange = useCallback((owner: string[]) => {
+    onChangeRef.current({ ...valueRef.current, owner });
+  }, []);
+
+  const handleStageChange = useCallback((stage: string[]) => {
+    onChangeRef.current({ ...valueRef.current, stage });
+  }, []);
+
   return (
     <>
-      <EuiFlexGroup>
+      <EuiFlexGroup gutterSize="xs">
         <EuiFlexItem grow={1}>
-          <AlgorithmSelector value={algorithm} onChange={onAlgorithmsChange} fullWidth />
-        </EuiFlexItem>
-        <EuiFlexItem grow={2}>
-          <RangePicker
-            start={trainedStart}
-            end={trainedEnd}
-            onStartChange={onTrainedStartChange}
-            onEndChange={onTrainedEndChange}
-            placeholders={['Train Start Time', 'Train End Time']}
+          <EuiFieldSearch
+            defaultValue={defaultSearch}
+            fullWidth
+            placeholder="Search by name, person, or keyword"
+            onSearch={handleSearch}
           />
         </EuiFlexItem>
+        <EuiFlexItem grow={false}>
+          <EuiFilterGroup>
+            <TagFilter value={value.tag} onChange={handleTagChange} />
+            <OwnerFilter value={value.owner} onChange={handleOwnerChange} />
+            <StageFilter value={value.stage} onChange={handleStageChange} />
+          </EuiFilterGroup>
+        </EuiFlexItem>
       </EuiFlexGroup>
-      <ContextSelector algorithm={algorithm} value={context} onChange={onContextChange} />
     </>
   );
 };
