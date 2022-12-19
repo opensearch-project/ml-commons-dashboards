3c388e7b04e2b7b052c1868a4b082dc4542b9844
Lin Wang <wonglam@amazon.com>
2022-12-19 16:25:30 +0800

    Feature/update model list UI (#27)
    * feat: add model aggerate search API
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>
    
    * feat: update model table ui
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>
    
    * refactor: remove unused opensearch.createClient
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>

--- server/routes/constants.ts
+++ server/routes/constants.ts
@@ -10,6 +10,7 @@ export const MODEL_LOAD_API_ENDPOINT = `${API_PREFIX}/model/load`;
 export const MODEL_UNLOAD_API_ENDPOINT = `${API_PREFIX}/model/unload`;
 export const MODEL_PROFILE_API_ENDPOINT = `${API_PREFIX}/model/profile`;
 export const MODEL_UPLOAD_API_ENDPOINT = `${MODEL_API_ENDPOINT}/upload`;
+export const MODEL_AGGREGATE_API_ENDPOINT = `${API_PREFIX}/model-aggregate`;
 export const TASK_API_ENDPOINT = `${API_PREFIX}/task`;
 export const TRAIN_API_ENDPOINT = `${API_PREFIX}/train`;
 export const MODEL_ALGORITHM_API_ENDPOINT = `${API_PREFIX}/model-algorithm`;
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -8,3 +8,4 @@ export { taskRouter } from './task_router';
 export { trainRouter } from './train_router';
 export { modelAlgorithmRouter } from './model_algorithm_router';
 export { predictRouter } from './predict_router';
+export { modelAlgregateRouter } from './model_aggregate_router';
