7f1021c3a0c2c9cbb9de154cff4c2114395d80e5
xyinshen <xyinshen@amazon.com>
2022-12-25 13:31:33 +0800

    xys_update_12/25
    Signed-off-by: xyinshen <xyinshen@amazon.com>

--- public/components/register_model_type_modal/index.tsx
+++ public/components/register_model_type_modal/index.tsx
@@ -4,7 +4,7 @@
  */
 import { EuiSpacer } from '@elastic/eui';
 import React, { useState } from 'react';
-
+import { useHistory } from 'react-router-dom';
 import {
   EuiButton,
   EuiModal,
@@ -17,54 +17,16 @@ import {
   EuiCheckableCard,
   EuiText,
   EuiComboBox,
-  EuiSelect,
-  EuiSuperSelect,
-  EuiHealth,
   EuiComboBoxOptionOption,
 } from '@elastic/eui';
 import { htmlIdGenerator } from '@elastic/eui';
 import { Link } from 'react-router-dom';
-import { ROUTES } from '../../../common/router';
+
 export function RegisterModelTypeModal(props: any) {
   const { getMsg } = props;
   const msg: string = 'close';
-  const [isModalVisible, setIsModalVisible] = useState(false);
-
-  const closeModal = () => setIsModalVisible(false);
-  const showModal = () => setIsModalVisible(true);
   const radioName = htmlIdGenerator()();
-  const [radio, setRadio] = useState('radio2');
-  const [nestedRadio, setNestedRadio] = useState('nestedRadio1');
-  const radios = [
-    {
-      id: 'radios0',
-      label: 'Option one',
-    },
-    {
-      id: 'radios1',
-      label: 'Option two',
-    },
-    {
-      id: 'radios2',
-      label: 'Option three',
-      disabled: true,
-    },
-  ];
-  const nestedRadios = [
-    {
-      id: 'nestedRadio1',
-      label: 'Nested option one',
-    },
-    {
-      id: 'nestedRadio2',
-      label: 'Nested option two',
-    },
-    {
-      id: 'nestedRadio3',
-      label: 'Nested option three',
-    },
-  ];
-  let modal;
+  const [radio, setRadio] = useState('ownModal');
   const options = [
     {
       label: 'Titan',
@@ -98,16 +60,21 @@ export function RegisterModelTypeModal(props: any) {
       label: 'Hyperion',
     },
   ];
-  // const options = [
-  //   { value: 'option_one', text: 'Option one' },
-  //   { value: 'option_two', text: 'Option two' },
-  //   { value: 'option_three', text: 'Option three' },
-  // ];
   const onChange = (selectedOptions: EuiComboBoxOptionOption[]) => {
     // We should only get back either 0 or 1 options.
     setSelected(selectedOptions);
   };
+  const history = useHistory();
   const [selectedOptions, setSelected] = useState<EuiComboBoxOptionOption[]>([]);
+  const handleContinue = (radioSelected: any) => {
+    if (radioSelected === 'fromRepo') {
+      history.push(
+        `/model-registry/register-model?name=${selectedOptions[0]?.label}&version=${selectedOptions[0]?.label}`
+      );
+    } else {
+      history.push('/model-registry/register-model');
+    }
+  };
   return (
     <div>
       <EuiModal onClose={() => getMsg(msg)} maxWidth="1000px">
@@ -135,9 +102,9 @@ export function RegisterModelTypeModal(props: any) {
                     </div>
                   }
                   name={radioName}
-                  value="radio1"
-                  checked={radio === 'radio1'}
-                  onChange={() => setRadio('radio1')}
+                  value="fromRepo"
+                  checked={radio === 'fromRepo'}
+                  onChange={() => setRadio('fromRepo')}
                 />
               </EuiFlexItem>
               <EuiFlexItem>
@@ -154,9 +121,9 @@ export function RegisterModelTypeModal(props: any) {
                     </div>
                   }
                   name={radioName}
-                  value="radio2"
-                  checked={radio === 'radio2'}
-                  onChange={() => setRadio('radio2')}
+                  value="ownModal"
+                  checked={radio === 'ownModal'}
+                  onChange={() => setRadio('ownModal')}
                 />
               </EuiFlexItem>
             </EuiFlexGroup>
@@ -164,7 +131,7 @@ export function RegisterModelTypeModal(props: any) {
         </EuiModalBody>
         <EuiSpacer />
         <EuiSpacer />
-        <EuiModalBody style={{ display: radio === 'radio1' ? 'block' : 'none' }}>
+        <EuiModalBody style={{ display: radio === 'fromRepo' ? 'block' : 'none' }}>
           <strong>Select model from repository</strong>
           <EuiSpacer />
           <EuiComboBox
@@ -174,39 +141,22 @@ export function RegisterModelTypeModal(props: any) {
             selectedOptions={selectedOptions}
             onChange={onChange}
           />
-
-          {/* <EuiSuperSelect
-            options={options}
-            valueOfSelected={value}
-            onChange={(value) => onChange(value)}
-          /> */}
-          {/* <EuiSelect
-            id="selectDocExample"
-            options={options}
-            value={value}
-            onChange={(e) => onChange(e)}
-            aria-label="Use aria labels when no actual label is in use"
-            hasNoInitialSelection={true}
-            placeholder="Select modal"
-          /> */}
         </EuiModalBody>
         <EuiModalFooter>
           <EuiButton color="primary" iconSide="right" onClick={() => getMsg(msg)}>
             {/* <EuiButton color="primary" iconSide="right" onClick={() => getMsg(msg)}> */}
             Cancle
           </EuiButton>
-
-          <Link
-            to={
-              radio === 'radio1'
-                ? `/model-registry/register-model?name=${selectedOptions[0]?.label}&version=${selectedOptions[0]?.label}`
-                : '/model-registry/register-model'
-            }
+          <EuiButton
+            color="primary"
+            fill
+            iconSide="right"
+            onClick={() => {
+              handleContinue(radio);
+            }}
           >
-            <EuiButton color="primary" fill iconSide="right" onClick={() => {}}>
-              Continue
-            </EuiButton>
-          </Link>
+            Continue
+          </EuiButton>
         </EuiModalFooter>
         {/* <div>{{selectedOptions}}</div> */}
       </EuiModal>
