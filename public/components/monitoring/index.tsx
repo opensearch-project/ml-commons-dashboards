81fc58d6cbb0bd8a611cd217f431d4efa4680284
Yulong Ruan <ruanyl@amazon.com>
2022-12-29 11:10:33 +0800

    feat: added an empty "Deployed models" panel
    Signed-off-by: Yulong Ruan <ruanyl@amazon.com>

--- public/components/monitoring/index.tsx
+++ public/components/monitoring/index.tsx
@@ -1,10 +1,16 @@
-import { EuiPageHeader } from '@elastic/eui';
+import { EuiPanel, EuiPageHeader, EuiTitle, EuiSpacer } from '@elastic/eui';
 import React from 'react';
 
 export const Monitoring = () => {
   return (
     <div>
       <EuiPageHeader pageTitle="Monitoring" />
+      <EuiSpacer size="m" />
+      <EuiPanel>
+        <EuiTitle size="s">
+          <h3>Deployed models</h3>
+        </EuiTitle>
+      </EuiPanel>
     </div>
   );
 };
