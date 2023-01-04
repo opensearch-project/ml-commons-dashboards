be97ae36261379fb1637832d3c5037ed14c295f8
xyinshen <xyinshen@amazon.com>
2023-01-04 15:13:17 +0800

    fix:banner
    Signed-off-by: xyinshen <xyinshen@amazon.com>

--- public/components/banner/index.tsx
+++ public/components/banner/index.tsx
@@ -3,15 +3,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 /* Vector */
-// import { EuiIcon } from '@elastic/eui';
-// export const Banner=()=>{
-//     return (
-//         <div>
-
-//         </div>
-//     );
-// }
-
 import React from 'react';
 
 import { EuiCallOut, EuiIcon } from '@elastic/eui';
@@ -24,14 +15,15 @@ export const Banner = () => {
           The feature is experimental and should not be used in a productuion environment.
         </span>
         <span>
-          For more information,see the Machine Learing Monitoring Documentation{' '}
-          <EuiIcon type="popout" style={{ color: 'blue' }} />.
+          For more information,see{' '}
+          <span style={{ color: '#006bb4' }}>Machine Learing Monitoring Documentation</span>
+          <EuiIcon type="popout" style={{ color: '#006bb4' }} />.
         </span>
 
         <p>
           <span>To leave feedback,</span>
-          <span style={{ color: 'blue' }}>visit forum.opensearch.org</span>
-          <EuiIcon type="popout" style={{ color: 'blue' }} />
+          <span style={{ color: '#006bb4' }}>visit forum.opensearch.org</span>
+          <EuiIcon type="popout" style={{ color: '#006bb4' }} />
         </p>
       </EuiCallOut>
     </div>
