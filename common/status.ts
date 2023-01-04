9fb7d07ad32eb2c776cefda88cac80f8b04390ad
tygao <tygao@amazon.com>
2023-01-04 10:57:33 +0800

    feat: add status filter (#34)
    * feat: init status filter
    
    Signed-off-by: raintygao <tygao@amazon.com>
    
    * feat: add test for status filter
    
    Signed-off-by: raintygao <tygao@amazon.com>
    
    * fix: fix unit tes typo error
    
    Signed-off-by: raintygao <tygao@amazon.com>
    
    * fix: eui popover onclose not emit
    
    Signed-off-by: raintygao <tygao@amazon.com>
    
    * chore(test): add params validate for unit test
    
    Signed-off-by: raintygao <tygao@amazon.com>
    
    Signed-off-by: raintygao <tygao@amazon.com>

---
+++ common/status.ts
@@ -0,0 +1,8 @@
+export const STATUS_FILTER = [
+  {
+    label: 'Responding',
+    color: '#017D73',
+  },
+  { label: 'Partially responding', color: '#F5A700' },
+  { label: 'Not responding', color: '#343741' },
+];
