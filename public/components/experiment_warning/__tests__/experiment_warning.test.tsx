81ee2b9bf9b753be4dc5f126bea044669274f547
xyinshen <xyinshen@amazon.com>
2023-01-05 19:55:43 +0800

    feat: adminui_banner_updated
    Signed-off-by: xyinshen <xyinshen@amazon.com>

--- public/components/experiment_warning/__tests__/experiment_warning.test.tsx
+++ public/components/experiment_warning/__tests__/experiment_warning.test.tsx
@@ -3,22 +3,12 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 import React from 'react';
-import userEvent from '@testing-library/user-event';
 import { render, screen } from '../../../../test/test_utils';
 import { ExperimentalWarning } from '../';
-async function setup({ onUpdateFilters = jest.fn() }) {
-  const user = userEvent.setup({});
+it('should navigete to # when link is clicked ', () => {
   render(<ExperimentalWarning />);
-  // open popover
-  await user.click(
-    screen.getByText('Machine Learing Monitoring Documentation' || 'visit forum.opensearch.org')
+  const link = screen.getByText(
+    'Machine Learning Monitoring Documentation' || 'visit forum.opensearch.org'
   );
-  return { user };
-}
-
-it('should navigete to # when link is clicked ', async () => {
-  const onUpdateFilters = jest.fn();
-  const { user } = await setup({ onUpdateFilters });
-  await user.click(screen.getByText('Status'));
-  expect(onUpdateFilters).toHaveAttribute('href', '#');
+  expect(link.getAttribute('href')).toBe('#');
 });
