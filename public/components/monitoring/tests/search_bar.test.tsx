928708c393092f9271970d10c7a53932c1ff72e3
xyinshen <xyinshen@amazon.com>
2023-01-12 18:31:06 +0800

    feat: searbar_test_updated
    Signed-off-by: xyinshen <xyinshen@amazon.com>

--- public/components/monitoring/tests/search_bar.test.tsx
+++ public/components/monitoring/tests/search_bar.test.tsx
@@ -12,24 +12,24 @@ describe('<SearchBar />', () => {
     render(<SearchBar onSearch={() => {}} value="foo" />);
     expect(screen.getByPlaceholderText('Search by name or ID')).toBeInTheDocument();
   });
-  it('should searchbar changed when value changed', () => {
-    let value = 'foo';
-    render(<SearchBar onSearch={() => {}} value={value} />);
-    expect(screen.getByDisplayValue('foo')).toBeInTheDocument();
-    value = 'test';
-    render(<SearchBar onSearch={() => {}} value={value} />);
-    expect(screen.getByDisplayValue('test')).toBeInTheDocument();
+  it('should searchbar changed when value changed', async () => {
+    const { rerender } = render(<SearchBar onSearch={() => {}} value="" />);
+    const user = userEvent.setup();
+    await user.type(screen.getByLabelText('Search by name or ID'), 'foo');
+    expect(screen.getByLabelText('Search by name or ID')).toHaveValue('foo');
+    const value = '';
+    await rerender(<SearchBar onSearch={() => {}} value={value} />);
+    expect(screen.getByLabelText('Search by name or ID')).not.toHaveValue();
   });
-
-  it('should callback onSearch in 200ms intervel when type', async () => {
+  it('should callback onSearch in 200ms when type', async () => {
     jest.useFakeTimers();
     const searchByNameOrId = jest.fn();
+    const user = userEvent.setup({ advanceTimers: jest.advanceTimersByTime });
     render(<SearchBar onSearch={searchByNameOrId} value="foo" />);
-    const $e = screen.getByPlaceholderText('Search by name or ID');
-    await userEvent.type($e, 'foo');
+    await user.type(screen.getByPlaceholderText('Search by name or ID'), 'foo');
     expect(searchByNameOrId).not.toHaveBeenCalled();
-
     jest.advanceTimersByTime(200);
     expect(searchByNameOrId).toHaveBeenCalled();
+    jest.useRealTimers();
   });
 });
