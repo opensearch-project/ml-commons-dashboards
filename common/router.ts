f484901c2dc0e551286948191425c06bb257b736
wanglam <wonglam@amazon.com>
2023-01-19 14:17:46 +0800

    feat: remove all prototype code (#56)
    Signed-off-by: Lin Wang <wonglam@amazon.com>
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>

--- common/router.ts
+++ common/router.ts
@@ -22,43 +22,10 @@ export const ROUTES: RouteConfig[] = [
 
 /* export const ROUTES1 = [
   {
-    path: routerPaths.train,
-    Component: Train,
-    label: 'Train Model',
-    icon: 'createSingleMetricJob',
-  },
-  {
     path: routerPaths.modelList,
     Component: ModelList,
     label: 'Model List',
     icon: 'createSingleMetricJob',
-  },
-  {
-    path: routerPaths.predict,
-    Component: Predict,
-    label: 'Predict',
-    icon: 'regressionJob',
-  },
-  {
-    path: '/',
-    Component: Home,
-    label: 'Home',
-    exact: true,
-  },
-  {
-    path: routerPaths.taskList,
-    Component: TaskList,
-    label: 'Task List',
-    icon: 'createSingleMetricJob',
-  },
-  {
-    path: routerPaths.modelDetail,
-    Component: ModelDetail,
-  },
-  {
-    path: routerPaths.modelUpload,
-    label: 'Model Upload',
-    Component: ModelUpload,
   },
   {
     path: routerPaths.registerModel,
