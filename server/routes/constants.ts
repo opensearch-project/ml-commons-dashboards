d7e92f7ebc49f01a2d62c3d64861f07df69a6df0
Lin Wang <wonglam@amazon.com>
2023-01-28 14:01:26 +0800

    feat: remove model registry related code and dependencies (#63)
    Signed-off-by: Lin Wang <wonglam@amazon.com>

--- server/routes/constants.ts
+++ server/routes/constants.ts
@@ -6,10 +6,5 @@
 export const API_PREFIX = '/api/ml-commons';
 
 export const MODEL_API_ENDPOINT = `${API_PREFIX}/model`;
-export const MODEL_LOAD_API_ENDPOINT = `${API_PREFIX}/model/load`;
-export const MODEL_UNLOAD_API_ENDPOINT = `${API_PREFIX}/model/unload`;
-export const MODEL_PROFILE_API_ENDPOINT = `${API_PREFIX}/model/profile`;
-export const MODEL_UPLOAD_API_ENDPOINT = `${MODEL_API_ENDPOINT}/upload`;
-export const MODEL_AGGREGATE_API_ENDPOINT = `${API_PREFIX}/model-aggregate`;
 export const PROFILE_API_ENDPOINT = `${API_PREFIX}/profile`;
 export const DEPLOYED_MODEL_PROFILE_API_ENDPOINT = `${PROFILE_API_ENDPOINT}/deployed-model`;
