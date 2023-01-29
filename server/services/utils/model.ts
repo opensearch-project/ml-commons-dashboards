12da8b250f6f05aa6d5e3cc477ad92b89516fc37
wanglam <wonglam@amazon.com>
2023-01-29 14:26:06 +0800

    fix: no node count data in model search (#65)
    Signed-off-by: Lin Wang <wonglam@amazon.com>

--- server/services/utils/model.ts
+++ server/services/utils/model.ts
@@ -6,20 +6,6 @@
 import { MODEL_STATE } from '../../../common';
 import { generateTermQuery } from './query';
 
-export const convertModelSource = (source: {
-  model_content: string;
-  name: string;
-  algorithm: string;
-  model_state: string;
-  model_version: string;
-}) => ({
-  content: source.model_content,
-  name: source.name,
-  algorithm: source.algorithm,
-  state: source.model_state,
-  version: source.model_version,
-});
-
 export const generateModelSearchQuery = ({
   states,
   nameOrId,
