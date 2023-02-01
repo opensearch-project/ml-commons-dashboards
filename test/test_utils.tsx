c4621da413275c40f3cf596c6eeaab2e6e40bc34
xyinshen <xyinshen@amazon.com>
2023-02-01 13:51:43 +0800

    feat: add model_register_button to model_list
    Signed-off-by: xyinshen <xyinshen@amazon.com>

--- test/test_utils.tsx
+++ test/test_utils.tsx
@@ -2,7 +2,6 @@
  * Copyright OpenSearch Contributors
  * SPDX-License-Identifier: Apache-2.0
  */
-
 import React, { FC, ReactElement } from 'react';
 import { I18nProvider } from '@osd/i18n/react';
 import { Provider as ReduxProvider } from 'react-redux';
