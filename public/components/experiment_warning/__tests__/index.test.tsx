28ae2cd7aaee6eda23637a7d992a9b02c2156bad
Lin Wang <wonglam@amazon.com>
2023-02-06 11:45:08 +0800

    fix: experiment banner copy and spaces
    Signed-off-by: Lin Wang <wonglam@amazon.com>

--- public/components/experiment_warning/__tests__/index.test.tsx
+++ public/components/experiment_warning/__tests__/index.test.tsx
@@ -7,9 +7,9 @@ import { render, screen } from '../../../../test/test_utils';
 import { ExperimentalWarning } from '..';
 
 describe('<ExperimentalWarning />', () => {
-  it('should navigate to # when clicking link of `Machine Learning Monitoring Documentation`', () => {
+  it('should navigate to # when clicking link of "Machine Learning Documentation"', () => {
     render(<ExperimentalWarning />);
-    const link = screen.getByText('Machine Learning Monitoring Documentation');
+    const link = screen.getByText('Machine Learning Documentation');
     expect(link.getAttribute('href')).toBe('#');
   });
 
