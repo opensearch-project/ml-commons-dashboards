6eabebe044502501fd123036bb56049cc2ae1884
Yulong Ruan <ruanyl@amazon.com>
2023-02-14 20:54:56 +0800

    feat: add register form submission footer (#99)
    + Form footer will stick at the bottom of the screen
    + Display error counts in the footer
    
    Signed-off-by: Yulong Ruan <ruanyl@amazon.com>
    Signed-off-by: Lin Wang <wonglam@amazon.com>

--- public/components/register_model/register_model.tsx
+++ public/components/register_model/register_model.tsx
@@ -6,7 +6,19 @@
 import React, { useCallback, useEffect } from 'react';
 import { FieldErrors, useForm, FormProvider } from 'react-hook-form';
 import { useParams } from 'react-router-dom';
-import { EuiPageHeader, EuiSpacer, EuiForm, EuiButton, EuiPanel, EuiText } from '@elastic/eui';
+import {
+  EuiPageHeader,
+  EuiSpacer,
+  EuiForm,
+  EuiButton,
+  EuiPanel,
+  EuiText,
+  EuiBottomBar,
+  EuiFlexGroup,
+  EuiFlexItem,
+} from '@elastic/eui';
+import useObservable from 'react-use/lib/useObservable';
+import { from } from 'rxjs';
 
 import { ModelDetailsPanel } from './model_details';
 import type { ModelFileFormData, ModelUrlFormData } from './register_model.types';
@@ -19,6 +31,7 @@ import { APIProvider } from '../../apis/api_provider';
 import { upgradeModelVersion } from '../../utils';
 import { useSearchParams } from '../../hooks/use_search_params';
 import { isValidModelRegisterFormType } from './utils';
+import { useOpenSearchDashboards } from '../../../../../src/plugins/opensearch_dashboards_react/public';
 
 const DEFAULT_VALUES = {
   name: '',
@@ -28,9 +41,16 @@ const DEFAULT_VALUES = {
   tags: [{ key: '', value: '' }],
 };
 
+const FORM_ID = 'mlModelUploadForm';
+
 export const RegisterModelForm = () => {
   const { id: latestVersionId } = useParams<{ id: string | undefined }>();
   const typeParams = useSearchParams().get('type');
+
+  const {
+    services: { chrome },
+  } = useOpenSearchDashboards();
+  const isLocked = useObservable(chrome?.getIsNavDrawerLocked$() ?? from([false]));
 
   const formType = isValidModelRegisterFormType(typeParams) ? typeParams : 'upload';
   const partials =
@@ -80,9 +100,12 @@ export const RegisterModelForm = () => {
     console.log(errors);
   }, []);
 
+  const errorCount = Object.keys(form.formState.errors).length;
+
   return (
     <FormProvider {...form}>
       <EuiForm
+        id={FORM_ID}
         data-test-subj="mlCommonsPlugin-registerModelForm"
         onSubmit={form.handleSubmit(onSubmit, onError)}
         component="form"
@@ -102,15 +125,37 @@ export const RegisterModelForm = () => {
               <EuiSpacer size="xl" />
             </React.Fragment>
           ))}
-          <EuiButton
-            disabled={form.formState.isSubmitting}
-            isLoading={form.formState.isSubmitting}
-            type="submit"
-            fill
-          >
-            Register model
-          </EuiButton>
         </EuiPanel>
+        <EuiSpacer size="xxl" />
+        <EuiSpacer size="xxl" />
+        <EuiBottomBar left={isLocked ? '320px' : 0}>
+          <EuiFlexGroup justifyContent="flexEnd">
+            {errorCount > 0 && (
+              <EuiFlexItem
+                style={{
+                  justifyContent: 'center',
+                  marginRight: 'auto',
+                }}
+                grow={false}
+              >
+                <EuiText style={{ padding: '0 8px', borderLeft: '4px solid #BD271E' }}>
+                  {errorCount} form {errorCount > 1 ? 'errors' : 'error'}
+                </EuiText>
+              </EuiFlexItem>
+            )}
+            <EuiFlexItem grow={false}>
+              <EuiButton
+                form={FORM_ID}
+                disabled={form.formState.isSubmitting}
+                isLoading={form.formState.isSubmitting}
+                type="submit"
+                fill
+              >
+                {latestVersionId ? 'Register version' : 'Register model'}
+              </EuiButton>
+            </EuiFlexItem>
+          </EuiFlexGroup>
+        </EuiBottomBar>
       </EuiForm>
     </FormProvider>
   );
