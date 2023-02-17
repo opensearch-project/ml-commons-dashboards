215684d6b7799e601317f50a22c2873d985af7e3
Lin Wang <wonglam@amazon.com>
2023-02-17 16:00:16 +0800

    feat: disable plugin by default
    Signed-off-by: Lin Wang <wonglam@amazon.com>

--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { PluginInitializerContext } from '../../../src/core/server';
+import { schema } from '@osd/config-schema';
+
+import { PluginConfigDescriptor, PluginInitializerContext } from '../../../src/core/server';
 import { MlCommonsPlugin } from './plugin';
 
 // This exports static code and TypeScript types,
@@ -14,3 +16,9 @@ export function plugin(initializerContext: PluginInitializerContext) {
 }
 
 export { MlCommonsPluginSetup, MlCommonsPluginStart } from './types';
+
+export const config: PluginConfigDescriptor = {
+  schema: schema.object({
+    enabled: schema.boolean({ defaultValue: false }),
+  }),
+};
