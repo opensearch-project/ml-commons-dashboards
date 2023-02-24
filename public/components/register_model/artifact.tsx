0f9feb5b67202b3800dfe41c8be595604a052171
Yulong Ruan <ruanyl@amazon.com>
2023-02-24 11:31:23 +0800

    feat: update artifact file validation rules (#118)
    + increase model file size limit to 4GB
    + add model file extension validation to only allow ZIP file
    
    ---------
    
    Signed-off-by: Yulong Ruan <ruanyl@amazon.com>
    Signed-off-by: Lin Wang <wonglam@amazon.com>

--- public/components/register_model/artifact.tsx
+++ public/components/register_model/artifact.tsx
@@ -6,8 +6,9 @@
 import React, { useState } from 'react';
 import { EuiTitle, htmlIdGenerator, EuiSpacer, EuiText, EuiRadio, EuiLink } from '@elastic/eui';
 
-import { ModelFileUploader } from './artifact_file';
+import { MAX_MODEL_FILE_SIZE, ModelFileUploader } from './artifact_file';
 import { ArtifactUrl } from './artifact_url';
+import { ONE_GB } from '../../../common/constant';
 
 export const ArtifactPanel = () => {
   const [selectedSource, setSelectedSource] = useState<'source_from_computer' | 'source_from_url'>(
@@ -21,8 +22,8 @@ export const ArtifactPanel = () => {
       </EuiTitle>
       <EuiText style={{ maxWidth: 450 }}>
         <small>
-          Provide the model artifact for upload. If uploading from local file, keep your browser
-          open until the upload is complete.{' '}
+          The zipped artifact must include a model file and a tokenizer file. If uploading with a
+          local file, keep this browser open util the upload completes.{' '}
           <EuiLink href="http://www.example.com" external>
             Learn more
           </EuiLink>
@@ -48,6 +49,17 @@ export const ArtifactPanel = () => {
       <EuiSpacer size="m" />
       {selectedSource === 'source_from_computer' && <ModelFileUploader />}
       {selectedSource === 'source_from_url' && <ArtifactUrl />}
+      <EuiSpacer size="xs" />
+      <EuiText size="xs" color="subdued">
+        Accepted file format: ZIP (.zip). Maximum size, {MAX_MODEL_FILE_SIZE / ONE_GB}GB.
+      </EuiText>
+      <EuiText size="xs" color="subdued">
+        The ZIP mush include the following contents:
+        <ul>
+          <li>Model File, accepted formats: Torchscript(.pt), ONNX(.onnx)</li>
+          <li>Tokenizer file, accepted format: JSON(.json)</li>
+        </ul>
+      </EuiText>
     </div>
   );
 };
