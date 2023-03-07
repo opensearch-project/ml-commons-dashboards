ea0d891fd2d0c41200eb371eaedb0230477e41f0
xyinshen <xyinshen@amazon.com>
2023-03-07 12:56:43 +0800

    fix: add help button unit-test
    Signed-off-by: xyinshen <xyinshen@amazon.com>

--- public/components/register_model/__tests__/register_model_configuration.test.tsx
+++ public/components/register_model/__tests__/register_model_configuration.test.tsx
@@ -9,7 +9,7 @@ import { setup } from './setup';
 describe('<RegisterModel /> Configuration', () => {
   it('should render a help flyout when click help button', async () => {
     const { user } = await setup();
-
+    
     expect(screen.getByLabelText('Configuration in JSON')).toBeInTheDocument();
     await user.click(screen.getByTestId('model-configuration-help-button'));
     expect(screen.getByRole('dialog')).toBeInTheDocument();
