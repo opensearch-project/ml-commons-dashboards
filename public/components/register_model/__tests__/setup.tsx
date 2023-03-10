00a2431de6dd91131985c36e70bd4dfb91a1b07d
Lin Wang <wonglam@amazon.com>
2023-03-10 15:12:03 +0800

    Featuer/fill pre trained model data to register form (#135)
    * feat: clear observable object after error catched
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>
    
    * feat: update pre-trained model config data
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>
    
    * feat: fill pre-trained name description configuration in model import form
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>
    
    * feat: add loading screen for importing pre-trained model form
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>
    
    * feat: add todo for failed to load pre-trained model
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>
    
    ---------
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>

--- public/components/register_model/__tests__/setup.tsx
+++ public/components/register_model/__tests__/setup.tsx
@@ -8,12 +8,17 @@ import userEvent from '@testing-library/user-event';
 
 import { RegisterModelForm } from '../register_model';
 import { Model } from '../../../apis/model';
-import { render, RenderWithRouteProps, screen } from '../../../../test/test_utils';
+import { render, RenderWithRouteProps, screen, waitFor } from '../../../../test/test_utils';
 
 jest.mock('../../../apis/task');
 
-export async function setup(options?: RenderWithRouteProps) {
+interface SetupOptions extends RenderWithRouteProps {
+  ignoreFillFields?: Array<'name' | 'description'>;
+}
+
+export async function setup(options?: SetupOptions) {
   render(<RegisterModelForm />, { route: options?.route ?? '/' });
+  await waitFor(() => expect(screen.queryByLabelText('Model Form Loading')).toBe(null));
   const nameInput = screen.getByLabelText<HTMLInputElement>(/^name$/i);
   const descriptionInput = screen.getByLabelText<HTMLTextAreaElement>(/description/i);
   const submitButton = screen.getByRole<HTMLButtonElement>('button', {
@@ -29,10 +34,15 @@ export async function setup(options?: RenderWithRouteProps) {
     data: [],
     pagination: { totalRecords: 0, currentPage: 1, pageSize: 1, totalPages: 0 },
   });
+
   // fill model name
-  await user.type(nameInput, 'test model name');
+  if (!options?.ignoreFillFields?.includes('name')) {
+    await user.type(nameInput, 'test model name');
+  }
   // fill model description
-  await user.type(descriptionInput, 'test model description');
+  if (!options?.ignoreFillFields?.includes('description')) {
+    await user.type(descriptionInput, 'test model description');
+  }
   // fill model file
   if (modelFileInput) {
     await user.upload(
