09022d5644a3212007dafda2445aeb6754ae7364
Yulong Ruan <ruanyl@amazon.com>
2023-03-20 18:02:27 +0800

    test: initiate the use of MSW for API mocking
    This commit initiate the use of MSW for API mocking, model repository
    APIs are now mocked by MSW and the usages of jest.mock are removed
    accordingly
    
    Signed-off-by: Yulong Ruan <ruanyl@amazon.com>

--- test/setupTests.ts
+++ test/setupTests.ts
@@ -4,3 +4,16 @@
  */
 
 import 'babel-polyfill';
+import fetch, { Request, Response } from 'node-fetch';
+
+import { setupDashboard } from './setup_dashboard';
+
+// @ts-ignore
+global.Request = Request;
+
+// @ts-ignore
+global.Response = Response;
+
+global.fetch = fetch;
+
+setupDashboard();
