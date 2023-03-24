6422ccc91a3dcf0bb1260702545fb2e5075b6bdb
Lin Wang <wonglam@amazon.com>
2023-03-24 16:52:38 +0800

    feat: add model version page component
    Signed-off-by: Lin Wang <wonglam@amazon.com>

---
+++ public/components/model_version/__tests__/model_version.test.tsx
@@ -0,0 +1,69 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import React from 'react';
+import { generatePath, Route } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+
+import { ModelVersion } from '../model_version';
+import { render, screen, waitFor, mockOffsetMethods } from '../../../../test/test_utils';
+import { routerPaths } from '../../../../common/router_paths';
+
+const setup = () =>
+  render(
+    <Route path={routerPaths.modelVersion}>
+      <ModelVersion />
+    </Route>,
+    { route: generatePath(routerPaths.modelVersion, { id: '1' }) }
+  );
+
+describe('<ModelVersion />', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should display consistent model name and version', async () => {
+    setup();
+
+    await waitFor(() => {
+      expect(screen.getByText('model1')).toBeInTheDocument();
+      expect(screen.getByText('v1.0.0')).toBeInTheDocument();
+    });
+  });
+
+  it('should display loading screen during Model.getOne calling', async () => {
+    jest.useFakeTimers();
+
+    setup();
+
+    expect(screen.getByTestId('modelVersionLoadingSpinner')).toBeInTheDocument();
+    jest.advanceTimersByTime(3000);
+    jest.useRealTimers();
+    await waitFor(() => {
+      expect(screen.queryByTestId('modelVersionLoadingSpinner')).not.toBeInTheDocument();
+    });
+  });
+
+  it('should display v1.0.1 and update location.pathname after version selected', async () => {
+    const mockRest = mockOffsetMethods();
+    const user = userEvent.setup();
+
+    setup();
+
+    await waitFor(() => {
+      expect(screen.getByText('v1.0.0')).toBeInTheDocument();
+    });
+    await user.click(screen.getByText('v1.0.0'));
+
+    await user.click(screen.getByText('1.0.1'));
+
+    await waitFor(() => {
+      expect(screen.getByText('v1.0.1')).toBeInTheDocument();
+    });
+    expect(location.pathname).toBe('/model-registry/model-version/2');
+
+    mockRest();
+  });
+});
