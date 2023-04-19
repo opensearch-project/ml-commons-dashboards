88ed4c6040add08d60552cd7399d7f98dfb77cd7
Lin Wang <wonglam@amazon.com>
2023-04-19 14:51:58 +0800

    feat: replace stage_filter with deployment toggle
    Signed-off-by: Lin Wang <wonglam@amazon.com>

--- public/components/model_list/__tests__/model_list_filter.test.tsx
+++ public/components/model_list/__tests__/model_list_filter.test.tsx
@@ -4,28 +4,55 @@
  */
 
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 
 import { ModelListFilter } from '../model_list_filter';
 import { render, screen } from '../../../../test/test_utils';
 
 describe('<ModelListFilter />', () => {
-  it('should render default search bar with tag, stage and owner filter', () => {
-    render(<ModelListFilter value={{ tag: [], stage: [], owner: [] }} onChange={() => {}} />);
+  it('should render default search bar with tag, deployed and owner filter', () => {
+    render(<ModelListFilter value={{ tag: [], owner: [] }} onChange={() => {}} />);
     expect(screen.getByPlaceholderText('Search by name, person, or keyword')).toBeInTheDocument();
     expect(screen.getByText('Tags')).toBeInTheDocument();
-    expect(screen.getByText('Stage')).toBeInTheDocument();
     expect(screen.getByText('Owner')).toBeInTheDocument();
+    expect(screen.getByText('Deployed')).toBeInTheDocument();
+    expect(screen.getByText('Undeployed')).toBeInTheDocument();
   });
 
   it('should render default search value and filter value', () => {
     render(
       <ModelListFilter
         defaultSearch="foo"
-        value={{ tag: ['tag1'], stage: ['stage1'], owner: ['owner1'] }}
+        value={{ tag: ['tag1'], owner: ['owner1'] }}
         onChange={() => {}}
       />
     );
     expect(screen.getByDisplayValue('foo')).toBeInTheDocument();
-    expect(screen.queryAllByText('1')).toHaveLength(3);
+    expect(screen.queryAllByText('1')).toHaveLength(2);
+    expect(screen.getByText('Deployed')).not.toHaveClass('euiFilterButton-hasActiveFilters');
+    expect(screen.getByText('Undeployed')).not.toHaveClass('euiFilterButton-hasActiveFilters');
+  });
+
+  it('should call onChange with consistent deployed value', async () => {
+    const onChangeMock = jest.fn();
+    const user = userEvent.setup();
+    const { rerender } = render(
+      <ModelListFilter value={{ tag: [], owner: [] }} onChange={onChangeMock} />
+    );
+
+    await user.click(screen.getByText('Deployed'));
+    expect(onChangeMock).toHaveBeenCalledWith(expect.objectContaining({ deployed: true }));
+
+    rerender(
+      <ModelListFilter value={{ tag: [], owner: [], deployed: true }} onChange={onChangeMock} />
+    );
+    await user.click(screen.getByText('Undeployed'));
+    expect(onChangeMock).toHaveBeenCalledWith(expect.objectContaining({ deployed: false }));
+
+    rerender(
+      <ModelListFilter value={{ tag: [], owner: [], deployed: false }} onChange={onChangeMock} />
+    );
+    await user.click(screen.getByText('Undeployed'));
+    expect(onChangeMock).toHaveBeenCalledWith(expect.objectContaining({ deployed: undefined }));
   });
 });
