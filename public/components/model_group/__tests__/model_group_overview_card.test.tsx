95b936cb3d74cc62cc681be1a92356fc85d036a9
Lin Wang <wonglam@amazon.com>
2023-04-26 10:59:22 +0800

    Feature/update model detail page layout (#166)
    * feat: add model_group related panels and cards
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>
    
    * feat: add cards and panels to model group detail
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>
    
    * test: add TZ=UTC to avoid timezone error in runner
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>
    
    ---------
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>

---
+++ public/components/model_group/__tests__/model_group_overview_card.test.tsx
@@ -0,0 +1,40 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import React from 'react';
+
+import { render, screen, within } from '../../../../test/test_utils';
+import { ModelGroupOverviewCard } from '../model_group_overview_card';
+
+describe('<ModelGroupOverviewCard />', () => {
+  it('should model group overview information according passed data', () => {
+    render(
+      <ModelGroupOverviewCard
+        id="model-1-id"
+        description="Model description of model 1"
+        isModelOwner
+        owner="Foo"
+        createdTime={1682324310318}
+        updatedTime={1682342310318}
+      />
+    );
+
+    expect(screen.getByText('Model description of model 1')).toBeInTheDocument();
+    expect(screen.getByText('Foo (you)')).toBeInTheDocument();
+    expect(screen.getByText('Created')).toBeInTheDocument();
+    expect(
+      within(screen.getByText('Created').closest('dl')!).getByText('Apr 24, 2023 8:18 AM')
+    ).toBeInTheDocument();
+    expect(screen.getByText('Last updated')).toBeInTheDocument();
+    expect(
+      within(screen.getByText('Last updated').closest('dl')!).getByText('Apr 24, 2023 1:18 PM')
+    ).toBeInTheDocument();
+
+    expect(screen.getByText('model-1-id')).toBeInTheDocument();
+    expect(
+      within(screen.getByText('model-1-id')).getByTestId('copy-id-button')
+    ).toBeInTheDocument();
+  });
+});
