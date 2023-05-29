177b14b6ad0579452120d44bd72a8b1b0652a148
Lin Wang <wonglam@amazon.com>
2023-05-29 16:32:01 +0800

    feat: remove experiment warning banner (#194)
    * feat: remove experiment warning banner
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>
    
    * doc: update release notes for remove experiment banner
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>
    
    ---------
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>
    (cherry picked from commit 0ef4753d120aea661c9a4caff734edf51d6de3e5)

--- public/components/monitoring/index.tsx
+++ public/components/monitoring/index.tsx
@@ -16,7 +16,6 @@ import React, { useState, useRef, useCallback } from 'react';
 
 import { RefreshInterval } from '../common/refresh_interval';
 import { PreviewPanel } from '../preview_panel';
-import { ExperimentalWarning } from '../experiment_warning';
 import { ModelDeploymentItem, ModelDeploymentTable } from './model_deployment_table';
 import { useMonitoring } from './use_monitoring';
 import { ModelStatusFilter } from './model_status_filter';
@@ -69,7 +68,6 @@ export const Monitoring = () => {
 
   return (
     <div>
-      <ExperimentalWarning />
       <EuiSpacer size="s" />
       <EuiSpacer size="xs" />
       <EuiPageHeader
