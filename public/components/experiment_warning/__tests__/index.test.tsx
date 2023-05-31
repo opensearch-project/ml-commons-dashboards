eb871989cfc1bce45f7745f0067503ec94db8982
Lin Wang <wonglam@amazon.com>
2023-05-31 08:07:50 +0800

    Revert "feat: remove experiment warning banner (#194)"
    This reverts commit 0ef4753d120aea661c9a4caff734edf51d6de3e5.
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>

---
+++ public/components/experiment_warning/__tests__/index.test.tsx
@@ -0,0 +1,25 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import React from 'react';
+import { render, screen } from '../../../../test/test_utils';
+import { ExperimentalWarning } from '..';
+
+describe('<ExperimentalWarning />', () => {
+  it('should navigate to # when clicking link of "Machine Learning Documentation"', () => {
+    render(<ExperimentalWarning />);
+    const link = screen.getByText('Machine Learning Documentation');
+    expect(link.getAttribute('href')).toBe(
+      'https://opensearch.org/docs/latest/ml-commons-plugin/ml-dashbaord/'
+    );
+  });
+
+  it('should navigate to # when clicking forum.opensearch.org', () => {
+    render(<ExperimentalWarning />);
+    const link = screen.getByText('forum.opensearch.org');
+    expect(link.getAttribute('href')).toBe(
+      'https://forum.opensearch.org/t/feedback-ml-commons-ml-model-health-dashboard-for-admins-experimental-release/12494'
+    );
+  });
+});
