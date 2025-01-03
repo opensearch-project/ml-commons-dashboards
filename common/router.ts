e767c0ce0412255dbb3f30bd34d4d1e9d2bd469f
Lin Wang <wonglam@amazon.com>
2025-01-03 11:29:06 +0800

    Hide model registry entrances
    Signed-off-by: Lin Wang <wonglam@amazon.com>

--- common/router.ts
+++ common/router.ts
@@ -3,11 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { Model } from '../public/components/model';
-import { ModelList } from '../public/components/model_list';
 import { Monitoring } from '../public/components/monitoring';
-import { RegisterModelForm } from '../public/components/register_model/register_model';
-import { ModelVersion } from '../public/components/model_version';
 import { routerPaths } from './router_paths';
 
 interface RouteConfig {
@@ -28,43 +24,4 @@ export const ROUTES: RouteConfig[] = [
     label: 'Overview',
     nav: true,
   },
-  {
-    path: routerPaths.registerModel,
-    label: 'Register Model',
-    Component: RegisterModelForm,
-    nav: true,
-  },
-  {
-    path: routerPaths.modelList,
-    label: 'Model Registry',
-    Component: ModelList,
-    nav: true,
-  },
-  {
-    path: routerPaths.model,
-    // TODO: refactor label to be dynamic so that we can display group name in breadcrumb
-    label: 'Model',
-    Component: Model,
-    nav: false,
-  },
-  {
-    path: routerPaths.modelVersion,
-    label: 'Model Version',
-    Component: ModelVersion,
-    nav: false,
-  },
 ];
-
-/* export const ROUTES1 = [
-  {
-    path: routerPaths.modelList,
-    Component: ModelList,
-    label: 'Model List',
-    icon: 'createSingleMetricJob',
-  },
-  {
-    path: routerPaths.registerModel,
-    label: 'Register Model',
-    Component: RegisterModelForm,
-  },
-];*/
